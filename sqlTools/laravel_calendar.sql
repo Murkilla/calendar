d3a06b204d96930121fb475fdf36da5c61ff166c
Joey Yang <joey93627@gmail.com>
2021-11-15 18:28:22 +0800

    fixed laravel_calendar.sql

--- sqlTools/laravel_calendar.sql
+++ sqlTools/laravel_calendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:3306
--- 產生時間： 2021 年 11 月 15 日 17:54
+-- 產生時間： 2021 年 11 月 15 日 18:26
 -- 伺服器版本： 10.3.31-MariaDB-0ubuntu0.20.04.1
 -- PHP 版本： 7.4.3
 
@@ -19,16 +19,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `laravel`
+-- 資料庫： `laravel_calendar`
 --
+CREATE DATABASE IF NOT EXISTS `laravel_calendar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `laravel_calendar`;
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `calendar`
+-- 資料表結構 `laravel_calendar`
 --
 
-CREATE TABLE `calendar` (
+CREATE TABLE `laravel_calendar` (
   `id` int(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `start` datetime NOT NULL,
@@ -37,10 +39,10 @@ CREATE TABLE `calendar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 傾印資料表的資料 `calendar`
+-- 傾印資料表的資料 `laravel_calendar`
 --
 
-INSERT INTO `calendar` (`id`, `title`, `start`, `end`, `switch`) VALUES
+INSERT INTO `laravel_calendar` (`id`, `title`, `start`, `end`, `switch`) VALUES
 (1, 'qazss', '2021-11-16 00:00:00', '2021-11-17 00:00:00', 'Y'),
 (2, 'zxc', '2021-11-23 00:00:00', '2021-11-24 00:00:00', 'Y'),
 (3, 'asd', '2021-11-30 00:00:00', '2021-12-01 00:00:00', 'Y');
@@ -50,9 +52,9 @@ INSERT INTO `calendar` (`id`, `title`, `start`, `end`, `switch`) VALUES
 --
 
 --
--- 資料表索引 `calendar`
+-- 資料表索引 `laravel_calendar`
 --
-ALTER TABLE `calendar`
+ALTER TABLE `laravel_calendar`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -60,9 +62,9 @@ ALTER TABLE `calendar`
 --
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `calendar`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `laravel_calendar`
 --
-ALTER TABLE `calendar`
+ALTER TABLE `laravel_calendar`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
